ad8d8e4bae97903895f47a09a6659e5a08eae577
dream-unity <michael.revolution321@gmail.com>
2026-09-04 22:09:54 +1000

    Bust cache for Heart Mind Body navigation

--- runtime/loader.js
+++ runtime/loader.js
@@ -75,7 +75,7 @@
     window.__DREAM_UNITY_DOMAIN_NAV_LOADING__ = true;
     const navigationScript = document.createElement("script");
     const navigationUrl = new URL("../portal-subnav.js", loaderUrl);
-    navigationUrl.searchParams.set("v", "nine-domains-20260904-1");
+    navigationUrl.searchParams.set("v", "heart-mind-body-20260904-2");
     navigationScript.src = navigationUrl.href;
     navigationScript.async = false;
     navigationScript.onerror = () => console.error("Dream Unity domain navigation could not load");
